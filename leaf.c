82b1b29cb1c837c25efbdcac31815b055b1e1f48
Andrei-Darius Bodrogean <bodrogean.andrei@gmail.com>
2025-08-18 14:55:22 +0300

    Changed the exit key combination to CTRL-X. Completed the raw mode tranzition

--- leaf.c
+++ leaf.c
@@ -1,25 +1,60 @@
 #include <unistd.h>
 #include <termios.h>
+#include <errno.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+
+#define CTRL_KEY(k) ((k) & 0x1f)                                // The CTRL_KEY macro bitwise-ANDs a character with the value 00011111
+                                                                // It mirrors what the ctrl key does in the terminal: it strips bits 5 and 6 from whatever key you pressed in the combination with ctrl
 
 struct termios original_termios;
 
+void die( const char* s)                                        // function used for error handling
+{
+    perror(s);
+    exit(1);
+}
+
 void disableRawMode()
 {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios);      //when exiting, we reset the terminal to its original state
+    if( tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios) == -1 ) 
+        die("tcsetattr");                                        //when exiting, we reset the terminal to its original state
 }
 
 void enableRawMode()
 {
-    tcgetattr(STDIN_FILENO, &original_termios);                  // read the attributes in a struct 
+    if( tcgetattr(STDIN_FILENO, &original_termios) == -1 )       // read the attributes in a struct 
+        die("tcgetattr");                  
     atexit(disableRawMode);                                      // register our disableRawMode() function to be called automatically when the program exits 
-   
+    
     struct termios to_raw = original_termios;                    // copy of the initial state of the terminal
-    //The ECHO feature causes each key you type to be printed to the terminal, so you can see what you’re typing.
-    to_raw.c_lflag &= ~( ECHO | ICANON );                        // change the attribute by hand + c_lglah = dumping ground for other state
+    //The ECHO feature causes each key you type to be printed to the terminal, so you can see what you’re typing. 
+
+    
+    to_raw.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);                                  // c_iflag responsible for input
+    //IXON - I comes from input flag and XON is a combination of the two things we want to stop Ctrl-S = XOFF and Ctrl-Q = XON
+    //ICRNL - I stands for “input flag”, CR stands for “carriage return”, and NL stands for “new line”. It turns out that the terminal is helpfully translating any carriage returns (13, '\r') inputted by the user into newlines (10, '\n')
+    //see more at https://www.man7.org/linux/man-pages/man3/termios.3.html
+    
+    to_raw.c_lflag &= ~( ECHO | ICANON | IEXTEN| ISIG );        // change the attribute by hand + c_lglah = dumping ground for other state
     //ECHO=00000000000000000000000000001000 in binary. It is a bitflag
     //ICANON is not an input flag even though the majority of input flags start with I. It is a local flag in c_lflag
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &to_raw);                 // set back the attributes
+    //ISIG - flag responsible for SIGINT and SIGTST. I want to be able to exit the text editor using only the x! combination
+    //IEXTEN - flag responsible for the CTRL-V combination
+    
+    to_raw.c_oflag &= ~(OPOST);                                  // c_oflag responsible for output
+    //OPOST - O means output flag an POST stands for "post processing of output". We will turn off all output processing feature by turning off this flag.
+    
+    to_raw.c_cflag |= (CS8);                                     // c_cflag used to set the character size 
+    //CS8 - character size mask. It sets the character size (CS) to 8 bits per byte.
+    
+    to_raw.c_cc[VMIN] = 0;                                       // c_cc - control characters which control various terminal settings
+    to_raw.c_cc[VTIME] = 1;
+    //VMIN - value sets the minimum number of bytes of input needed before read() can return
+    //VTIME - value sets the maximum amount of time to wait before read() returns. It is in tenths of a second, so we set it to 100 milliseconds.
+    if( tcsetattr(STDIN_FILENO, TCSAFLUSH, &to_raw) == -1 )      // set back the attributes
+        die("tcsetattr");
     //With this part, we no longer see on the screen the keys we pressed
 }
 
@@ -28,13 +63,22 @@ int main()
 {
     enableRawMode();
     //we now want to be able to take input from the users keyboard
-    char ant, pos;
-    while( read(STDIN_FILENO, &ant, 1) == 1  )          //reads one byte (character) from the keyboard until there are
+              
+    while(1)                                            //we changed such that the terminal is not waiting for some input
     {
+        char ant = '\0';
+        if( read(STDIN_FILENO, &ant, 1) == -1 && errno != EAGAIN ) 
+            die("read");
+        if( iscntrl(ant) )
+        {
+            printf("%d\r\n", ant);
+        }
+        else{
+            printf("%d ('%c')\r\n", ant, ant);
+        }
         if( ant == '\n') continue;  
-        if( ant == '!' && pos == 'x' )                  //you can exit by entering x!
+        if( ant == CTRL_KEY('x'))                       // changed ther exit condition to CTRL x 
             break;
-        pos = ant;
     }        
 					                                    //no more characters to be read
     return 0;
